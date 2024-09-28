60410725d2c0239162a1160e263eb650cf8d46cc
Aaron Kennedy <aaron@dmkdesign.ca>
2024-09-27 20:18:16 -0700

    Update ModalRemote.js

--- src/assets/ModalRemote.js
+++ src/assets/ModalRemote.js
@@ -116,7 +116,21 @@ function ModalRemote(modalId, sidebarOptions=false) {
 
     this.isVisible = function()
     {
-        return  $(this.modal).is(':visible');
+        var win = $(window);
+
+        var viewport = {
+            top : win.scrollTop(),
+            left : win.scrollLeft()
+        };
+        viewport.right = viewport.left + win.width();
+        viewport.bottom = viewport.top + win.height();
+    
+        var bounds = this.offset();
+        bounds.right = bounds.left + this.outerWidth();
+        bounds.bottom = bounds.top + this.outerHeight();
+    
+        return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
+       // return  $(this.modal).is(':visible');
     }
     /**
      * Hide the modal
